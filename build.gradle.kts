05c4a32dce3e6482e92002cc67a3768fd32652c2
Duncan Calvert <dcalvert@ihmc.us>
2017-11-10 13:18:07 -0600

    Convert JOctoMap to new build plugin.

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+   id("us.ihmc.ihmc-build") version "0.10.4"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "1.6.6"
+   openSource = true
+   //maintainter = "Sylvain Bertrand"
+   
+   configureDependencyResolution()
+   configurePublications()
+}
+
+dependencies {
+   compile("us.ihmc:euclid-core:0.4.6")
+   compile("org.apache.commons:commons-math3:3.3")
+   compile("org.apache.commons:commons-lang3:3.3")
+   compile("org.ejml:dense64:0.30")
+}
